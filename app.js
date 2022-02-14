476f5ca59b1caa5c2fa64782733e7725ee131d1f
jambrose0 <jambrose0518@gmail.com>
2022-02-14 14:03:30 -0500

    index.html is now generated, and code in modular

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArgs.length; i++) {
-    console.log(profileDataArgs[i]);
-  }
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-  console.log("===========");
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
 
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+  console.log("Portfolio complete!  Check out index.html to see the output!");
+});
