2efbfbfbaaf7e103644705258cb16fb2e9a4283e
jambrose0 <jambrose0518@gmail.com>
2022-02-20 15:07:27 -0500

    can generate work projects, with option to be featured or not featured

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const inquirer = require("inquirer");
 
+const fs = require("fs");
+const generatePage = require("./src/page-template");
+
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -48,7 +51,6 @@ const promptUser = () => {
       },
     },
   ]);
-  // .then((answers) => console.log(promptUser));
 };
 
 const promptProject = (portfolioData) => {
@@ -132,16 +134,12 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw err;
+
+      console.log(
+        "Portfolio complete!  Check out index.html to see the output!"
+      );
+    });
   });
-
-// const fs = require("fs");
-// const generatePage = require("./src/page-template");
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("./index.html", pageHTML, (err) => {
-//   if (err) throw err;
-
-//   console.log("Portfolio complete!  Check out index.html to see the output!");
-// });
