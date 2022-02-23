752d4a40b5c996daeeaae91b7f8b3e5a4b04dc9b
jambrose0 <jambrose0518@gmail.com>
2022-02-22 20:58:13 -0500

    working on copying file in a new working page

--- app.js
+++ app.js
@@ -134,12 +134,35 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw err;
-
-      console.log(
-        "Portfolio complete!  Check out index.html to see the output!"
-      );
-    });
+    return generatePage(portfolioData);
+  })
+  .then((pageHTML) => {
+    return fs.writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
   });
+
+// const pageHTML = generatePage(portfolioData);
+// fs.writeFile("./dist/index.html", pageHTML, (err) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log(
+//     "Page created! Check out index.html in this directory to see it!"
+//   );
+
+//   fs.copyFile("./src/style.css", "./dist/style.css", (err) => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log("Style sheet copied successfully!");
